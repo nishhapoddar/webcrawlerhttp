cece29baddaef021cda2783ef14d5c0900b18d91
nishhapoddar <nishapoddar404@gmail.com>
2026-05-24 07:39:31 -0700

    crawl single page   with error handling

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("Hellp world!");
+const {crawlpage} = require(`./crawl.js`)
+function main(){
+    if(process.argv.length <3){  //it is 3 because when we run the command node main.js https://www.google.com then in process.argv we have 3 things first is node, second is main.js and third is url
+        console.log('no website provided')
+        process.exit(1)
+    }
+    if(process.argv.length >3){
+        console.log('too many command line argv')
+        process.exit(1)
+    }
+    const baseURL = process.argv[2]
+    console.log(`starting crawl at ${baseURL}`)
+    crawlpage(baseURL)
+}
+main()
