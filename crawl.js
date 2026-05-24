d06c50d8f298be90b9c899d79a3f2332cde42ddf
nishhapoddar <nishapoddar404@gmail.com>
2026-05-24 06:57:06 -0700

    getURLsFROMHTML function and test

--- crawl.js
+++ crawl.js
@@ -1,3 +1,34 @@
+const{ JSDOM } = require('jsdom')
+
+function getURLsFROMHTML(htmlbody, baseURL) {
+    const urls = []
+    const dom = new JSDOM(htmlbody)
+    const linkELements= dom.window.document.querySelectorAll('a')
+    for(const linkElement of linkELements){
+        if(linkElement.href.slice(0,1)==='/'){
+            //relative url
+            try{
+                const urlobj =  new URL(`${baseURL}${linkElement.href}`)
+                urls.push(urlobj.href)
+            }catch(err){
+                console.log(`error with relative url: ${err.message}`)
+            }
+            
+        } else{
+            //absolute url
+            try{
+                const urlobj =  new URL(linkElement.href)
+                urls.push(urlobj.href)
+            }catch(err){
+                console.log(`error with absolute url: ${err.message}`)
+            }
+             
+            
+        }
+        
+    }
+    return urls
+}
 function normalizeURL(urlstring) {
     const urlobj = new URL(urlstring)
     const hostpath = `${urlobj.hostname}${urlobj.pathname}`
@@ -10,5 +41,6 @@ function normalizeURL(urlstring) {
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFROMHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,5 @@
-const { normalizeURL } = require('./crawl.js')
+// sometimes there can be many url that lead to same page so in that case we use this things to speacify that all the url are extually pointing to the same page.purpose of normalizeURL is to take us to the same thing.
+const { normalizeURL, getURLsFROMHTML } = require('./crawl.js')
 const { test, expect } = require('@jest/globals')
 
 test('normalizeURL strip protocol', () => {
@@ -28,5 +29,69 @@ test('normalizeURL strip protocol', () => {
     const expected = 'blog.boot.dev/path'
     expect(actual).toEqual(expected)
 })
+// getURLsFROMHTML is used to extract all the url from the html body and return them as an array of string
+test('getURLsFROMHTML absolute', () => {
+    const inputHTMLBody = `
+    <html>
+    <body>
+    <a href = "https://blog.boot.dev/path/"
+           Boot.dev BLOG
+           </a>
+           </body>
+           </html>
+    `
+    const inputbaseURL = "https://blog.boot.dev/path/"
+    const actual = getURLsFROMHTML(inputHTMLBody, inputbaseURL)
+    const expected = ["https://blog.boot.dev/path/"]
+    expect(actual).toEqual(expected)
+})
+test('getURLsFROMHTML relative', () => {
+    const inputHTMLBody = `
+    <html>
+    <body>
+    <a href = "/path/"
+           Boot.dev BLOG
+           </a>
+           </body>
+           </html>
+    `
+    const inputbaseURL = "https://blog.boot.dev"
+    const actual = getURLsFROMHTML(inputHTMLBody, inputbaseURL)
+    const expected = ["https://blog.boot.dev/path/"]
+    expect(actual).toEqual(expected)
+})
 
-// sometimes there can be many url that lead to same page so in that case we use this things to speacify that all the url are extually pointing to the same page.purpose of normalizeURL is to take us to the same thing.
+test('getURLsFROMHTML both', () => {
+    const inputHTMLBody = `
+    <html>
+    <body>
+    <a href = "https://blog.boot.dev/path1/"
+           Boot.dev BLOG path 1
+           </a>
+           <a href = "/path2/"
+           Boot.dev BLOG path 2
+           </a>
+           </body>
+           </html>
+    `
+    const inputbaseURL = "https://blog.boot.dev"
+    const actual = getURLsFROMHTML(inputHTMLBody, inputbaseURL)
+    const expected = ["https://blog.boot.dev/path1/", "https://blog.boot.dev/path2/"]
+    expect(actual).toEqual(expected)
+})
+test('getURLsFROMHTML relative', () => {
+    const inputHTMLBody = `
+    <html>
+    <body>
+    <a href = "invalid">
+           Invaid url
+           </a>
+           </body>
+           </html>
+    `
+    const inputbaseURL = "https://blog.boot.dev"
+    const actual = getURLsFROMHTML(inputHTMLBody, inputbaseURL)
+    const expected = []
+    expect(actual).toEqual(expected)
+})
+
